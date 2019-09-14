1d682fbaad961fc838f4cb6684f3eb45276f8e8a
Arslan Hayat <muhammadarslanhayat@gmail.com>
2019-09-14 14:10:17 +0500

    Add react-viz typins

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import Icon from "./components/Icon/Icon";
-import Card  from "./components/Card/Card";
-
+import Card from "./components/Card/Card";
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  LineSeries
+} from "react-vis";
 const App = () => {
   return (
-    <Card title="Default size card" extra={<Icon
-      icon={{ type: "star" }}
-      tooltip={{
-        title: () => <span>prompt text</span>
-      }}
-    />} width="30vw" height="50vh">
-     Content 
+    <Card
+      title="Default size card"
+      extra={
+        <Icon
+          icon={{ type: "star" }}
+          tooltip={{
+            title: () => <span>prompt text</span>
+          }}
+        />
+      }
+      width="30vw"
+      height="50vh"
+    >
+      <XYPlot width={300} height={100}>
+        <HorizontalGridLines />
+        <LineSeries
+          color="red"
+          data={[{ x: 1, y: 10 }, { x: 2, y: 5 }, { x: 3, y: 15 }]}
+        />
+        <XAxis title="X" />
+        <YAxis />
+      </XYPlot>
     </Card>
   );
 };
