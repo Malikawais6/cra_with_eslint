bc9213e39fc28fd0c4e5b7902196fc1c92798010
muhammadawais6 <malikxawais@gmail.com>
2019-09-26 18:15:45 +0500

    grid layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,18 +16,18 @@ export const Layout = () => {
   var layout = [
     { i: "a", x: 0, y: 0, w: 3.8, h: 9.2 },
     { i: "b", x: 3.9, y: 0, w: 3.8, h: 9.2 },
-    { i: "c", x: 7.8, y: 0, w: 3.8, h: 2.5 },
-    { i: "d", x: 7.8, y: 2, w: 3.8, h: 2.5 },
-    { i: "e", x: 7.8, y: 4, w: 3.8, h: 2.5 },
-    { i: "f", x: 7.8, y: 6, w: 3.8, h: 2.5 },
-    { i: "g", x: 0, y: 8, w: 3.8, h: 2.8 },
-    { i: "h", x: 3.9, y: 8, w: 3.8, h: 2.8 },
-    { i: "i", x: 7.8, y: 8, w: 3.8, h: 2.8 },
-    { i: "j", x: 0, y: 10, w: 3.8, h: 2.8 },
-    { i: "k", x: 3.9, y: 10, w: 3.8, h: 2.8 },
-    { i: "l", x: 7.8, y: 10, w: 3.8, h: 2.8 },
-    { i: "m", x: 0, y: 12, w: 3.8, h: 2.8 },
-    { i: "n", x: 3.9, y: 14, w: 3.8, h: 2.8 }
+    { i: "c", x: 7.8, y: 0, w: 3.8, h: 2.3 },
+    { i: "d", x: 7.8, y: 2, w: 3.8, h: 2.3 },
+    { i: "e", x: 7.8, y: 4, w: 3.8, h: 2.3 },
+    { i: "f", x: 7.8, y: 6, w: 3.8, h: 2.3 },
+    { i: "g", x: 0, y: 8, w: 3.8, h: 2.7 },
+    { i: "h", x: 3.9, y: 8, w: 3.8, h: 2.7 },
+    { i: "i", x: 7.8, y: 8, w: 3.8, h: 2.7 },
+    { i: "j", x: 0, y: 10, w: 3.8, h: 2.7 },
+    { i: "k", x: 3.9, y: 10, w: 3.8, h: 2.7 },
+    { i: "l", x: 7.8, y: 10, w: 3.8, h: 2.7 },
+    { i: "m", x: 0, y: 12, w: 3.8, h: 2.7 },
+    { i: "n", x: 3.9, y: 14, w: 3.8, h: 2.7 }
   ];
 
   return (
@@ -51,8 +51,6 @@ export const Layout = () => {
               }}
             />
           }
-          width="353px"
-          height="342px"
           threshold={20}
         >
           <CardVisual
@@ -76,8 +74,6 @@ export const Layout = () => {
               }}
             />
           }
-          width="353px"
-          height="342px"
         >
           <CardVisual
             showChart={true}
@@ -90,30 +86,19 @@ export const Layout = () => {
       </div>
 
       <div key="c" style={{ border: "solid" }}>
-        <Card title="Cost per code" width="353px" height="85px" showInfo={true}>
+        <Card title="Cost per code" showInfo={true}>
           <CardVisual numberSize={"medium"} data={DefaultCardData} />
         </Card>
       </div>
 
       <div key="d" style={{ border: "solid" }}>
-        <Card
-          title="Processing fee"
-          width="353px"
-          height="85px"
-          showInfo={true}
-        >
+        <Card title="Processing fee" showInfo={true}>
           <CardVisual numberSize={"medium"} data={DefaultCardData} />
         </Card>
       </div>
 
       <div key="e" style={{ border: "solid" }}>
-        <Card
-          title="SaaS"
-          width="353px"
-          height="85px"
-          threshold={20}
-          showInfo={true}
-        >
+        <Card title="SaaS" threshold={20} showInfo={true}>
           <CardVisual
             showInlineProgressBar={true}
             numberSize={"medium"}
@@ -123,13 +108,13 @@ export const Layout = () => {
       </div>
 
       <div key="f" style={{ border: "solid" }}>
-        <Card title="Miscallaneous" width="353px" height="85px">
+        <Card title="Miscallaneous">
           <CardVisual numberSize={"medium"} data={DefaultCardData} />
         </Card>
       </div>
 
       <div key="g" style={{ border: "solid" }}>
-        <Card title="Runway" width="353px" height="90px">
+        <Card title="Runway">
           <CardVisual numberSize={"large"} data={SimpleCard} />
         </Card>
       </div>
@@ -137,8 +122,6 @@ export const Layout = () => {
       <div key="h" style={{ border: "solid" }}>
         <Card
           title="Cash in Bank"
-          width="353px"
-          height="90px"
           extra={
             <Icon
               icon={{ component: () => <GraphIcon /> }}
@@ -153,24 +136,19 @@ export const Layout = () => {
       </div>
 
       <div key="i" style={{ border: "solid" }}>
-        <Card
-          title="Monthly budget"
-          width="353px"
-          height="90px"
-          showInfo={true}
-        >
+        <Card title="Monthly budget" showInfo={true}>
           <CardVisual numberSize={"large"} data={SimpleCard} />
         </Card>
       </div>
 
       <div key="j" style={{ border: "solid" }}>
-        <Card title="Floats with Brand" width="353px" height="90px">
+        <Card title="Floats with Brand">
           <CardVisual numberSize={"large"} data={DefaultCardData} />
         </Card>
       </div>
 
       <div key="k" style={{ border: "solid" }}>
-        <Card title="Stock held" width="353px" height="90px">
+        <Card title="Stock held">
           <CardVisual numberSize={"large"} data={DefaultCardData} />
         </Card>
       </div>
@@ -178,8 +156,6 @@ export const Layout = () => {
       <div key="l" style={{ border: "solid" }}>
         <Card
           title="Deposits"
-          width="353px"
-          height="85px"
           threshold={20}
           showInfo={true}
           extra={
@@ -202,8 +178,6 @@ export const Layout = () => {
       <div key="m" style={{ border: "solid" }}>
         <Card
           title="Average days float held"
-          width="353px"
-          height="90px"
           extra={
             <Icon
               icon={{ component: () => <GraphIcon /> }}
@@ -218,7 +192,7 @@ export const Layout = () => {
       </div>
 
       <div key="n" style={{ border: "solid" }}>
-        <Card title="days stock held" width="353px" height="90px">
+        <Card title="days stock held">
           <CardVisual numberSize={"large"} data={DefaultCardData} />
         </Card>
       </div>
