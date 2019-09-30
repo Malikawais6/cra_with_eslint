7a3fae0cce2d70a468a57314695958a26a4fab29
jabir.hussain <jabir.hussain@emumba.com>
2019-09-30 15:54:18 +0500

    Minor ui tweak

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -1,17 +1,24 @@
-import  { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 export const AppStyles = createGlobalStyle`
         html body {
           @import url("../src/assets/visualt/styles.css");
-            font-family: Visuelt;
-            font-style: normal;
-            font-stretch: normal;
-            line-height: normal; 
-            letter-spacing: normal;
+          font-family: Visuelt;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: normal; 
+          letter-spacing: normal;
+          background: #f0f2f5;
+          
+          #root {
+            > .ant-layout {
+              width: 1440px;
+              margin: 0 auto;
+            }
+          }
 
             .ant-layout-has-sider {
               min-height: calc(100vh - 62px);
-              min-width: 1440px;
               overflow-y: auto;
             }
         }
